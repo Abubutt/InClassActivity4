df7d3a871b91d6d42b0d1138ac61c0e58945ae71
Abu Butt <abubutt578@gmail.com>
2022-10-07 17:08:55 -0400

    Added code for in class activity 4

---
+++ InClassActivity4/Program.cs
@@ -0,0 +1,95 @@
+﻿interface IBookStore // interface
+{
+    List<Book> getBooks();
+    void addBook(Book item);
+    void removeBook(Book item);
+
+}
+
+class Book // class
+{
+    private string title = "";
+    private string author = "";
+
+    public string Title // property
+    {
+        get
+        {
+            return title;
+        }
+
+        set
+        {
+            title = value;
+        }
+    }
+
+    public string Author // property
+    {
+        get
+        {
+            return author;
+        }
+
+        set
+        {
+            author = value;
+        }
+    }
+}
+
+class BookStore : IBookStore // class using the IBookStore interface
+{
+    List<Book> books = new List<Book>();
+
+    public List<Book> getBooks() // method
+    {
+        return books;
+    }
+
+    public void addBook(Book item) //method
+    {
+        books.Add(item);
+    }
+    public void removeBook(Book item) // method
+    {
+        books.Remove(item);
+    }
+
+    public Book this[int i] // indexer
+    {
+        get { return books[i]; }
+        set { books[i] = value; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BookStore myStore = new BookStore();
+
+        Book book1 = new Book();
+        book1.Title = "Harry Potter";
+        book1.Author = "Jk Rowling";
+
+        Book book2 = new Book();
+        book2.Title = "Marvel Comics";
+        book2.Author = "Jacob Ress";
+
+        myStore.addBook(book1);
+        myStore.addBook(book2);
+
+        List<Book> bookList = myStore.getBooks();
+
+        foreach(var book in bookList)
+        {
+            Console.WriteLine(book.Title + " by " + book.Author);
+        }
+
+        Console.WriteLine(bookList[1].Title); // using index to get the book and then property Title to get the title of the book
+
+
+    }
+}
+
